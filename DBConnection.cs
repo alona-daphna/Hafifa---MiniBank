ab8ec83c21fe021e1ce92fee1f9ea66eece48375
Alona Daphna <alona.daphna@gmail.com>
2024-02-26 07:36:10 +0200

    feat: created DBConnection class

---
+++ DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient
+
+using System.Data;
+
+namespace MiniBank
+{
+    internal class DBConnection
+    {
+        private string ConnectionString { get; set; }
+        internal DBConnection(IConfiguration configuration)
+        {
+            ConnectionString = configuration.GetConnectionString("DB");
+        }
+
+        internal IDbConnection GetConnection() => new SqlConnection(ConnectionString);
+    }
+}
