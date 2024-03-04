d8ed3896dcf92492cabcb76ee3d110888636883c
Alona Daphna <alona.daphna@gmail.com>
2024-03-04 06:51:51 +0200

    feat: use nhibernate session in usercontroller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,110 +12,100 @@ namespace MiniBank.Controllers
         private DBConnection DBConnection { get; set; } = new DBConnection();
         private UserConverter UserConverter { get; set; } = new UserConverter();
         private ILogger Logger { get; set; } = MiniBankLogger.GetInstance().Logger;
+        private NhibernateConfig NhibernateConfig { get; set; } = NhibernateConfig.GetInstance();
 
-
-        internal Response<string> Create(string name, string password) 
+        internal Response<string?> Create(string name, string password) 
         {
-            using var conn = DBConnection.GetConnection();
-            conn.Open();
-
-            var command = new SqlCommand("INSERT Users (ID, Name, Password) VALUES (@ID, @Name, @Password)", conn);
-            var id = Guid.NewGuid().ToString();
-            command.Parameters.AddWithValue("ID", id);
-            command.Parameters.AddWithValue("Name", name);
-            command.Parameters.AddWithValue("Password", password);
-
             try
             {
-                command.ExecuteNonQuery();
+                var id = Guid.NewGuid().ToString();
+
+                using (var session = NhibernateConfig.SessionFactory.OpenSession())
+                {
+                    using var transaction = session.BeginTransaction();
+                    var user = new User { ID = id, Name = name, Password = password };
+                    session.Save(user);
+                    transaction.Commit();
+                }
+
                 Logger.Information("Created a new user with ID {id}", id);
 
-                return new Response<string> { Status = OperationStatus.Success, Data = id };
+                return new Response<string?> { Status = OperationStatus.Success, Data = id };
             }
             catch (SqlException ex)
             {
                 Logger.Error(ex, "Error in creating user.");
 
-                return new Response<string> { Status = OperationStatus.Error, ErrorMessage = "An error occurred unexpectedly. Please try again later." };
+                return new Response<string?> { Status = OperationStatus.Error, ErrorMessage = "An error occurred unexpectedly. Please try again later." };
             }
         }
 
         internal Response<User?> Delete(string id) 
         {
-            using var conn = DBConnection.GetConnection();
-            conn.Open();
-
-            var command = new SqlCommand("DELETE FROM Users WHERE ID = @ID", conn);
-            command.Parameters.AddWithValue("ID", id);
-
             try
             {
-                if (command.ExecuteNonQuery() == 0)
+                using (var session = NhibernateConfig.SessionFactory.OpenSession())
                 {
-                    return new Response<User?> { Status = OperationStatus.NotFound };
+                    var transaction = session.BeginTransaction();
+                    var user = session.Get<User?>(id);
+
+                    if (user == null)
+                    {
+                        return new Response<User?> { Status = OperationStatus.NotFound };
+                    }
+
+                    session.Delete(user);
+                    transaction.Commit();
+
+                    Logger.Information("Deleted user with ID {id}", id);
+
+                    return new Response<User?> { Status = OperationStatus.Success };
                 }
-
-                Logger.Information("Deleted user with ID {id}", id);
-
-                return new Response<User?> { Status = OperationStatus.Success };
-            } catch (SqlException ex)
+            }
+            catch (SqlException ex)
             {
-                Logger.Information("Error in deleting user with ID {id}", id);
+                Logger.Error(ex, "Error in deleting user with ID {id}", id);
 
                 return new Response<User?> { Status = OperationStatus.Error, ErrorMessage = "An error occurred unexpectedly. Please try again later." };
             }
         }
 
-        internal Response<List<User>> GetAll() 
+        internal Response<List<User?>> GetAll() 
         {
-            var users = new List<User>();
-
-            using var conn = DBConnection.GetConnection();
-           
-            conn.Open();
-
-            var command = new SqlCommand("SELECT ID, Name, Password FROM Users", conn);
-
             try
             {
-                using var reader = command.ExecuteReader();
-                
-                while (reader.Read())
-                {
-                    users.Add(UserConverter.Convert(reader));
-                }
+                using var session = NhibernateConfig.SessionFactory.OpenSession();
+                var users = session.Query<User?>().ToList();
 
-                return new Response<List<User>> { Status = OperationStatus.Success, Data = users };
+                return new Response<List<User?>> { Status = OperationStatus.Success, Data = users };
             } catch (SqlException ex)
             {
                 Logger.Error(ex, "Error in retrieving users");
 
-                return new Response<List<User>> { Status = OperationStatus.Error, ErrorMessage = "An error occurred unexpectedly. Please try again later." };
+                return new Response<List<User?>> { Status = OperationStatus.Error, ErrorMessage = "An error occurred unexpectedly. Please try again later." };
             }
         }
 
 
-        internal Response<User> GetByID(string id) 
+        internal Response<User?> GetByID(string id) 
         {
-            using var conn = DBConnection.GetConnection();
-            conn.Open();
-
-            var command = new SqlCommand("SELECT ID, Name, Password FROM Users WHERE ID = @ID", conn);
-            command.Parameters.AddWithValue("ID", id);
-
             try
             {
-                using var reader = command.ExecuteReader();
+                using var session = NhibernateConfig.SessionFactory.OpenSession();
+                var user = session.Get<User?>(id);
 
-                if (reader.Read())
+                if (user == null)
                 {
-                    return new Response<User> { Status = OperationStatus.Success, Data = UserConverter.Convert(reader) };
+                    return new Response<User?> { Status = OperationStatus.NotFound };
                 }
 
-                return new Response<User> { Status = OperationStatus.NotFound };
-            } catch (SqlException)
+                return new Response<User?> { Status = OperationStatus.Success, Data = user };
+
+            } catch (SqlException ex)
             {
-                return new Response<User> { Status = OperationStatus.Error, ErrorMessage = "An error occurred unexpectedly. Please try again later." };
+                Logger.Error(ex, "Error in retrieving user with ID {id}", id);
+
+                return new Response<User?> { Status = OperationStatus.Error, ErrorMessage = "An error occurred unexpectedly. Please try again later." };
             }
         }
     }
