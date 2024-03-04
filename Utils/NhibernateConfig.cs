2da759f40ceed91741c2a7a5ae54f5ca23d3ad0d
Alona Daphna <alona.daphna@gmail.com>
2024-03-04 06:13:16 +0200

    fix: add connection string to nhibernate config

--- Utils/NhibernateConfig.cs
+++ Utils/NhibernateConfig.cs
@@ -1,4 +1,10 @@
-﻿using NHibernate;
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Microsoft.Extensions.Configuration;
+using MiniBank.Mappings;
+using MiniBank.Models;
+using NHibernate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +15,32 @@ namespace MiniBank.Utils
 {
     internal class NhibernateConfig
     {
+        private static NhibernateConfig instance;
+        internal ISessionFactory SessionFactory { get; set; }
 
-        internal ISessionFactory GetSessiomFactory()
+
+        private NhibernateConfig()
         {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
+            var persistenceModel = AutoMap.AssemblyOf<User>()
+                .Where(t => t == typeof(User))
+                .Conventions.Add<PrimaryKeyConvention>();
+
+            SessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(configuration.GetConnectionString("DB")).ShowSql())
+                .Mappings(m => m.AutoMappings.Add(persistenceModel))
+                .BuildSessionFactory();
+        }
+
+        internal static NhibernateConfig GetInstance()
+        {
+            if ( instance == null )
+            {
+                instance = new NhibernateConfig();
+            }
+
+            return instance;
         }
     }
 }
